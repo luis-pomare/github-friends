9ff7ac10bafefd7efdd3f57fe09a51a9f5e6cc9c
unknown <luispomare92@gmail.com>
2022-07-24 12:15:51 -0500

    Fetch followers

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Card from '../card';
+import { getFriends } from '../../redux/reducers/friends';
+import urlStore from '../../redux/url';
+
+const { url, followers } = urlStore;
 
 const Home = () => {
   const friends = useSelector((state) => state.friends);
+  const dispatch = useDispatch();
   console.log(friends);
+
+  useEffect(() => {
+    if (friends.length === 0) {
+      const call = async (user = 'luis-pomare') => {
+        let response = await fetch(url + user + followers);
+        response = await response.json();
+        dispatch(getFriends(response));
+        console.log(friends);
+      };
+      call();
+    }
+    console.log(friends);
+  }, []);
+
   return (
     <main>
       <Card />
