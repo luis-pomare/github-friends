ffe06be671226df2ddc9e97a692b733681461c65
unknown <luispomare92@gmail.com>
2022-07-24 06:35:02 -0500

    Create featured section with h2 place holder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Featured from './components/featured';
 import Header from './components/header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Header />
+        <Featured />
         <Routes>
           <Route path="/" element={<Header />} />
         </Routes>
---
+++ src/components/featured.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Featured = () => (
+  <Link to="/details">
+    <section>
+      <h2>Featured</h2>
+    </section>
+  </Link>
+);
+
+export default Featured;
