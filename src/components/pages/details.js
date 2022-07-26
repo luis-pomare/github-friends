bbfa6eae825510036437866b27f9f5b094c570cc
unknown <luispomare92@gmail.com>
2022-07-26 17:08:51 -0500

    Add friend content

--- src/components/pages/details.js
+++ src/components/pages/details.js
@@ -2,11 +2,42 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const Details = () => {
-  const state = useSelector((state) => state);
+  const { friend } = useSelector((state) => state);
 
   return (
     <main>
-      <h2>{state.friend.login}</h2>
+      <span>
+        <img src={friend.avatar_url} alt="Friends avatar" />
+        <h2>{friend.login}</h2>
+        <p>
+          {friend.followers}
+          {' '}
+          followers
+        </p>
+      </span>
+      <h4>Friend information:</h4>
+      <span>
+        <h3>Following</h3>
+        <p>{friend.following}</p>
+      </span>
+      <span>
+        <h3>Active since</h3>
+        <p>{friend.created_at}</p>
+      </span>
+      <span>
+        <h3>Public repositiries</h3>
+        <p>{friend.public_repos}</p>
+      </span>
+      <span>
+        <h3>Public Gists</h3>
+        <p>{friend.public_gists}</p>
+      </span>
+      <span>
+        <h3>Public profile</h3>
+        <a href={friend.html_url} target="_blank" rel="noreferrer">
+          {friend.login}
+        </a>
+      </span>
     </main>
   );
 };
