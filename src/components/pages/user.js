2ad15bedcba2c70b64d0be53ca69fad701818066
unknown <luispomare92@gmail.com>
2022-07-27 14:25:57 -0500

    Create text input local state for user

--- src/components/pages/user.js
+++ src/components/pages/user.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const User = () => (
-  <form>
-    <Link to="/">back</Link>
-    <h2>Please enter your GitHub user name:</h2>
-    <input type="text" />
-  </form>
-);
+const User = () => {
+  const [state, setState] = useState('');
+  const handleChange = (e) => {
+    setState(e.target.value);
+  };
+  return (
+    <form>
+      <Link to="/">back</Link>
+      <h2>Please enter your GitHub user name:</h2>
+      <input type="text" value={state} onChange={handleChange} />
+    </form>
+  );
+};
 
 export default User;
