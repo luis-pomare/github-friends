404e0e12b7468cc2383deea8a8b7a003639af831
unknown <luispomare92@gmail.com>
2022-07-27 16:47:23 -0500

    Change user with hard code

--- src/components/pages/user.js
+++ src/components/pages/user.js
@@ -1,17 +1,41 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { getFollowers } from '../../redux/reducers/followers';
+import { getFollowings } from '../../redux/reducers/followings';
+import { getUser } from '../../redux/reducers/user';
+import { clearAll } from '../../redux/reducers/all';
 
 const User = () => {
   const [state, setState] = useState('');
+  const dispatch = useDispatch();
+
   const handleChange = (e) => {
     setState(e.target.value);
   };
+
+  const submitHandler = () => {
+    dispatch(getFollowers('BregornOriginal'));
+    dispatch(getFollowings('BregornOriginal'));
+    dispatch(getUser('BregornOriginal'));
+    dispatch(clearAll());
+    setState('');
+  };
+
   return (
-    <form>
+    <>
       <Link to="/">back</Link>
-      <h2>Please enter your GitHub user name:</h2>
-      <input type="text" value={state} onChange={handleChange} />
-    </form>
+      <h2>Please enter a valid GitHub user name:</h2>
+      <input
+        type="text"
+        value={state}
+        onChange={handleChange}
+        name="userInput"
+      />
+      <button type="button" id="submitUser" onClick={submitHandler}>
+        Submit
+      </button>
+    </>
   );
 };
 
