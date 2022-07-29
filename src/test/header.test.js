b2ddec82b26c61721b6ab0cb095f63f7a2452720
unknown <luispomare92@gmail.com>
2022-07-29 00:04:12 -0500

    Add conditional header testing

---
+++ src/test/header.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+
+import Header from '../components/header';
+
+describe('Header conditional tests', () => {
+  test('Home header displayed', () => {
+    render(
+      <BrowserRouter>
+        <Header caller='home' />
+      </BrowserRouter>
+    );
+    expect(document.querySelector('header')).toBeInTheDocument();
+  });
+
+  test('Details header displayed', () => {
+    render(
+      <BrowserRouter>
+        <Header caller='details' />
+      </BrowserRouter>
+    );
+    expect(document.querySelector('header')).toBeInTheDocument();
+  });
+
+  test('User header displayed', () => {
+    render(
+      <BrowserRouter>
+        <Header caller='user' />
+      </BrowserRouter>
+    );
+    expect(document.querySelector('header')).toBeInTheDocument();
+  });
+});
